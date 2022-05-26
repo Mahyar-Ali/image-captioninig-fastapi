1afc9c64872287ca083f5ef1a37acf08df6a6133
M.Mahyar Ali <mahyar@smodin.io>
2022-05-26 17:52:47 +0500

    added models and tokenizer

--- main.py
+++ main.py
@@ -1,49 +1,52 @@
 from base64 import encode
 from enum import Enum
 from unittest.mock import patch
-from fastapi import FastAPI, HTTPException, status
-
 
 import uvicorn
-from fastapi import File, UploadFile, FastAPI
+from fastapi import FastAPI, File, HTTPException, UploadFile, status
 
 app = FastAPI()
+
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
     try:
         contents = await file.read()
-        with open("images/"+file.filename, 'wb') as f:
+        with open("images/" + file.filename, "wb") as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         await file.close()
-        
+
     return {"message": f"Successfuly uploaded {file.filename}"}
 
 
 class EssayType(str, Enum):
     image_name = str
 
+
 from model import load_model
-encoder, decoder = load_model()
+
+encoder, decoder, tokenizer = load_model()
 
 from eval import evaluate, plot_attention
+
 
 @app.post("/predict")
 def predict(request: EssayType):
     request_dictionary = request.dict()
-    path = str("images/"+request_dictionary["image_name"])
+    path = str("images/" + request_dictionary["image_name"])
     try:
-        result,attention_plot = evaluate(path)
+        result, attention_plot = evaluate(path, encoder, decoder, tokenizer)
 
         attention_plot = plot_attention(path, result, attention_plot)
 
-        return {"result":result, "attention_plot":list(attention_plot)}
+        return {"result": result, "attention_plot": list(attention_plot)}
 
     except:
         return {"message": "Unexpected Error"}
+
 
 @app.get("/version")
 async def version():
--- model.py
+++ model.py
@@ -1,85 +1,103 @@
-#Importing the required Dependencies
-import tensorflow as tf
+# Importing the required Dependencies
+import json
+
 import numpy as np
-from src.config import embedding_dim, units, vocab_size,BATCH_SIZE, top_k, model_path
+import tensorflow as tf
 
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
-                                                  oov_token='<unk>',
-                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+from src.config import BATCH_SIZE, embedding_dim, model_path, top_k, units, vocab_size
+
+tokenizer = tf.keras.preprocessing.text.Tokenizer(
+    num_words=top_k, oov_token="<unk>", filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ '
+)
+
+
 class BahdanauAttention(tf.keras.Model):
-  def __init__(self,units):
-    super(BahdanauAttention,self).__init__()
-    self.W1 = tf.keras.layers.Dense(units)
-    self.W2 = tf.keras.layers.Dense(units)
-    self.V1 = tf.keras.layers.Dense(1)
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V1 = tf.keras.layers.Dense(1)
 
-  def call(self,features,hidden):
-    hidden_with_time_axis = tf.expand_dims(hidden,1)
-    score = tf.nn.tanh(self.W1(hidden_with_time_axis)+
-                       self.W2(features))
-    attention_weights = tf.nn.softmax(self.V1(score),axis=1)
+    def call(self, features, hidden):
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        score = tf.nn.tanh(self.W1(hidden_with_time_axis) + self.W2(features))
+        attention_weights = tf.nn.softmax(self.V1(score), axis=1)
 
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+        return context_vector, attention_weights
 
-    context_vector = attention_weights*features
-    context_vector = tf.reduce_sum(context_vector,axis=1)
-    return context_vector,attention_weights
 
 class CNN_Encoder(tf.keras.Model):
-  def __init__(self,embedding_dim):
-    super(CNN_Encoder,self).__init__()
-    self.fc = tf.keras.layers.Dense(embedding_dim)
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        self.fc = tf.keras.layers.Dense(embedding_dim)
 
-  def call(self,x):
-    x = self.fc(x)
-    x = tf.nn.tanh(x)
-    return x
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.tanh(x)
+        return x
+
 
 class RNN_Decoder(tf.keras.Model):
-  def __init__(self,embedding_dim,units,vocab_size):
-    super(RNN_Decoder,self).__init__()
-    self.units = units
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
 
-    self.embedding = tf.keras.layers.Embedding(vocab_size,embedding_dim)
-    self.GRU = tf.keras.layers.GRU(self.units,
-                                   return_sequences=True,
-                                   return_state=True,
-                                   recurrent_initializer = 'glorot_uniform')
-    self.drop1 = tf.keras.layers.Dropout(0.4)
-    self.batch_norm = tf.keras.layers.BatchNormalization()
-    self.fc1 = tf.keras.layers.Dense(512)
-    self.fc2 = tf.keras.layers.Dense(vocab_size)
-    
-    self.attention = BahdanauAttention(self.units)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.GRU = tf.keras.layers.GRU(
+            self.units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer="glorot_uniform",
+        )
+        self.drop1 = tf.keras.layers.Dropout(0.4)
+        self.batch_norm = tf.keras.layers.BatchNormalization()
+        self.fc1 = tf.keras.layers.Dense(512)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
 
-  def call(self,x,features,hidden):
-    context_vector,attention_weights = self.attention(features,hidden)
-    x = self.embedding(x)
+        self.attention = BahdanauAttention(self.units)
 
-    x = tf.concat([tf.expand_dims(context_vector,1),x],axis=-1)
+    def call(self, x, features, hidden):
+        context_vector, attention_weights = self.attention(features, hidden)
+        x = self.embedding(x)
 
-    output,state = self.GRU(x)
-    x = self.fc1(output)
-    x = self.drop1(x)
-    x = self.batch_norm(x)
-    x = tf.reshape(x, (-1, x.shape[2]))
-    x = self.fc2(x)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
-    return x,state,attention_weights
+        output, state = self.GRU(x)
+        x = self.fc1(output)
+        x = self.drop1(x)
+        x = self.batch_norm(x)
+        x = tf.reshape(x, (-1, x.shape[2]))
+        x = self.fc2(x)
 
-  def reset_state(self,batch_sz):
-    return tf.zeros([batch_sz,self.units])
+        return x, state, attention_weights
+
+    def reset_state(self, batch_sz):
+        return tf.zeros([batch_sz, self.units])
+
 
 def load_model():
-    #Loss and Optimizer
+    # Loss and Optimizer
+    with open(f"{model_path}/tokenizer.json") as f:
+        data = json.load(f)
+        tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(data)
+
     encoder = CNN_Encoder(embedding_dim)
-    decoder = RNN_Decoder(embedding_dim,units,vocab_size)
-    optimizer=tf.keras.optimizers.Adam()
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,reduction='none')
-    #Initializing using Random Input
-    test_x = encoder(np.ones([1,64,2048]))
+    decoder = RNN_Decoder(embedding_dim, units, vocab_size)
+    optimizer = tf.keras.optimizers.Adam()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction="none"
+    )
+    # Initializing using Random Input
+    test_x = encoder(np.ones([1, 64, 2048]))
     encoder.load_weights(f"{model_path}/weights1.hdf5")
-    b = decoder.attention(test_x,decoder.reset_state(BATCH_SIZE))
-    c = decoder(tf.expand_dims([tokenizer.word_index['<start>']]*BATCH_SIZE,1),test_x,decoder.reset_state(BATCH_SIZE))
+    b = decoder.attention(test_x, decoder.reset_state(BATCH_SIZE))
+    c = decoder(
+        tf.expand_dims([tokenizer.word_index["<start>"]] * BATCH_SIZE, 1),
+        test_x,
+        decoder.reset_state(BATCH_SIZE),
+    )
     decoder.load_weights(f"{model_path}/weights2.hdf5")
     decoder.attention.load_weights(f"{model_path}/weights3.hdf5")
-    return encoder, decoder
+    return encoder, decoder, tokenizer
